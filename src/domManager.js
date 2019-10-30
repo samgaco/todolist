78a365876315d5ac686686e2798da755ecffcb2b
samuel <samuelgarciacompanys@gmail.com>
2019-10-30 16:52:49 +0100

    adding elements to the list and local storage congruency fixed

--- src/domManager.js
+++ src/domManager.js
@@ -4,39 +4,60 @@ import toDo from './task';
 const domManager = (() => {
   const content = document.querySelector('.content');
   const newProjectButton =  document.querySelector('#newproject-button');
+  const clearButton =  document.querySelector('#clear-button');
   const taskList = document.querySelector('.task-list')
   const newProjectIcon = document.createElement('button');
   const newTask = document.createElement('li').classList.add('li-task');
+  const liTasks = document.querySelector('.li-task')
 
+
+  const renderProjectTasks = (projectName) => {
+
+  JSON.parse(localStorageManager.projectList.getItem(localStorageManager.projectList.key(projectName))).forEach(element => {
+          
+    document.querySelector('.task-list').insertAdjacentHTML('beforeend', `
+    <li class="li-task">
+    <div class="task-row">
+        <i class="fas fa-check-square"></i>
+         <p>${element.description}</p>
+         <i class="far fa-times-circle"></i>
+    </div>
+    </li>`)
+
+  }) };
+
+  const cleanTasks = () =>{
+    taskList.innerHTML = ""
+  };
 
   const startApp = () => {
+
+    if(localStorageManager.projectList.length === 0){
+      localStorageManager.initiateMain();
+    }
+
+    renderProjectTasks(0);
 
     newProjectButton.addEventListener('click', function(){
         document.querySelector('.new-project-form').style.display = 'block';  }
     );
 
+
+    clearButton.addEventListener('click', function(){
+      cleanTasks();
+      localStorageManager.projectList.clear();
+    
+    }
+  );
+
     document.querySelector('#input-task').addEventListener('keypress', function (e) {
-      e.preventDefault();
       var key = e.which || e.keyCode;
       if (key === 13) { // 13 is enter
 
         let taskObj = toDo.createTodo(document.querySelector('#input-task').value);
         localStorageManager.addTaskList('Main', taskObj)
-
-        let i = 0;
-        while(i < localStorageManager.projectList.length){
-          console.log(localStorageManager.projectList.getItem(localStorageManager.projectList.key(i)))
-          document.querySelector('.task-list li:last-of-type').insertAdjacentHTML('afterend', `
-          <li class="li-task">
-              <i class="fas fa-check-square"></i>
-               <p>${localStorageManager.projectList.getItem(localStorageManager.projectList.key(i))}</p>
-               <i class="far fa-times-circle"></i>
-          </li>`
-        )
-        i += 1;
-        }
-
-
+        cleanTasks();
+        renderProjectTasks(0);
 
       }
     })
--- src/localStorageManager.js
+++ src/localStorageManager.js
@@ -2,27 +2,26 @@ const localStorageManager = (() => {
   let counter = 2;
   let projectList = window.localStorage;
 
-  console.log(projectList);
-
-  let mainList = [
-    {
-      description: "Do a couple of code reviews",
-      status: false,
-      id: 0
-    },
-    {
-      description: "Finish up with the current project",
-      status: false,
-      id: 1
-    },
-    {
-      description: "Work on a bunch of coding challenges",
-      status: false,
-      id: 2
-    }
-  ]
-
-  projectList.setItem('Main', JSON.stringify(mainList));
+  const initiateMain = () => {
+    let mainList = [
+      {
+        description: "Do a couple of code reviews",
+        status: false,
+        id: 0
+      },
+      {
+        description: "Finish up with the current project",
+        status: false,
+        id: 1
+      },
+      {
+        description: "Work on a bunch of coding challenges",
+        status: false,
+        id: 2
+      }
+    ]
+    projectList.setItem('Main', JSON.stringify(mainList));
+  };
 
   const addProjects  =  (name) => {
     projectList.setItem(name, []);
@@ -33,11 +32,11 @@ const localStorageManager = (() => {
     task.id = counter;
     let arr = JSON.parse(projectList[name]);
     arr.push(task);
-    projectList[name] = JSON.stringify(arr);
+    projectList.setItem(name, JSON.stringify(arr))    
     return projectList[name];
   };
 
-  return {projectList, addProjects, addTaskList};
+  return {initiateMain, projectList, addProjects, addTaskList};
 })();
 
 
